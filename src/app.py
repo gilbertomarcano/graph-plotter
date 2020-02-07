775af211ea9472ba4cd459b23e44de27d9a9d347
gilbertomarcano <marcanogilbertoa@gmail.com>
2020-02-07 11:33:12 -0400

    Start ignoring build folder

--- src/app.py
+++ src/app.py
@@ -3,15 +3,11 @@ import tkinter
 from gui import GUI
 from plotter import Plotter, Point2D, Line2D
 
-# Main class of the program
-def main():
+def start():
     root = tkinter.Tk()
     root.geometry("640x550")
     app = GUI(root)
     root.mainloop()
     
-if __name__ == '__main__':
-    main()
-
 
 
---
+++ src/main.py
@@ -0,0 +1,12 @@
+import app
+
+
+# Main class of the program
+def main():
+    app.start()
+    
+if __name__ == '__main__':
+    main()
+
+
+
